3fab4339f406e7f9c1709a9fa0d5598720722447
maierftw <gustavo.wmaier@gmail.com>
2019-09-04 17:53:33 -0300

    aula 26

---
+++ src/java/Login.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Desenvolvimento
+ */
+public class Login extends HttpServlet {
+
+   @Override
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException {
+      // obtendo os parametros de requisição
+      String email = request.getParameter("email");
+      String senha = request.getParameter("senha");
+
+      //comparar com os dados de login default
+      if (email.equals("adm@ifpr.edu.br") &&
+          senha.equals("admin")){
+         //salvando sucesso do login na sessao
+         request.getSession().setAttribute("logado", true);
+
+         //redirecionar para a pagina de produtos
+         response.sendRedirect("produtos.jsp");
+      }else{
+         //redirecionar para a tela de login
+         response.sendRedirect("login.jsp");
+    }
+
+   }
+}
+
